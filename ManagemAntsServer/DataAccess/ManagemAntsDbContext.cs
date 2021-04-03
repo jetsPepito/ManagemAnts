b7b9b10c5c791eec5b8449d9411f502ac8f80081
louis.legatt <legatt.louis@gmail.com>
2021-04-03 15:50:47 +0200

    add entity framework

---
+++ ManagemAntsServer/DataAccess/ManagemAntsDbContext.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace ManagemAntsServer.DataAccess
+{
+    public partial class ManagemAntsDbContext : DbContext
+    {
+        public ManagemAntsDbContext()
+        {
+        }
+
+        public ManagemAntsDbContext(DbContextOptions<ManagemAntsDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Project> Projects { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-8S3HNIV\\MTI;Initial Catalog=ManagemAntsDb;Trusted_Connection=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "French_CI_AS");
+
+            modelBuilder.Entity<Project>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.Property(e => e.Description)
+                    .HasMaxLength(5000)
+                    .IsUnicode(false)
+                    .HasColumnName("description");
+
+                entity.Property(e => e.Id)
+                    .ValueGeneratedOnAdd()
+                    .HasColumnName("id");
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(100)
+                    .IsUnicode(false)
+                    .HasColumnName("name");
+
+                entity.Property(e => e.Owner).HasColumnName("owner");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ ManagemAntsServer/DataAccess/Project.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace ManagemAntsServer.DataAccess
+{
+    public partial class Project
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public long Owner { get; set; }
+    }
+}

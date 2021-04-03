1b0d1607206e436a70e215aaeeaabce441153b99
louis.legatt <legatt.louis@gmail.com>
2021-04-03 15:19:54 +0200

    databse project

---
+++ ManagemAntsDatabase/dbo/Tables/Projects.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Projects] (
+    [id]          BIGINT         IDENTITY (1, 1) NOT NULL,
+    [name]        VARCHAR (100)  NOT NULL,
+    [description] VARCHAR (5000) NULL,
+    [owner]       BIGINT         NOT NULL
+);
+

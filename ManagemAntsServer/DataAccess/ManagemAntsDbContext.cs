15222a352576546656c0bfe5a41dc3af76eb55ba
louis.legatt <legatt.louis@gmail.com>
2021-04-07 09:55:34 +0200

    add tasks on db

--- ManagemAntsServer/DataAccess/ManagemAntsDbContext.cs
+++ ManagemAntsServer/DataAccess/ManagemAntsDbContext.cs
@@ -18,13 +18,14 @@ namespace ManagemAntsServer.DataAccess
         }
 
         public virtual DbSet<Project> Projects { get; set; }
+        public virtual DbSet<Task> Tasks { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=DESKTOP-8S3HNIV\\MTI;Initial Catalog=ManagemAntsDb;Trusted_Connection=True");
+                optionsBuilder.UseSqlServer("Data Source=.\\MTI;Initial Catalog=ManagemAntsDb;Trusted_Connection=True");
             }
         }
 
@@ -34,16 +35,12 @@ namespace ManagemAntsServer.DataAccess
 
             modelBuilder.Entity<Project>(entity =>
             {
-                entity.HasNoKey();
+                entity.Property(e => e.Id).HasColumnName("id");
 
                 entity.Property(e => e.Description)
                     .HasMaxLength(5000)
                     .IsUnicode(false)
                     .HasColumnName("description");
-
-                entity.Property(e => e.Id)
-                    .ValueGeneratedOnAdd()
-                    .HasColumnName("id");
 
                 entity.Property(e => e.Name)
                     .IsRequired()
@@ -52,6 +49,39 @@ namespace ManagemAntsServer.DataAccess
                     .HasColumnName("name");
 
                 entity.Property(e => e.Owner).HasColumnName("owner");
+            });
+
+            modelBuilder.Entity<Task>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.CreatedAt)
+                    .HasColumnType("date")
+                    .HasColumnName("created_at");
+
+                entity.Property(e => e.Description)
+                    .HasColumnType("text")
+                    .HasColumnName("description");
+
+                entity.Property(e => e.Duration).HasColumnName("duration");
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(200)
+                    .IsUnicode(false)
+                    .HasColumnName("name");
+
+                entity.Property(e => e.ProjectId).HasColumnName("project_id");
+
+                entity.Property(e => e.State).HasColumnName("state");
+
+                entity.Property(e => e.TimeSpent).HasColumnName("time_spent");
+
+                entity.HasOne(d => d.Project)
+                    .WithMany(p => p.Tasks)
+                    .HasForeignKey(d => d.ProjectId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK_Tasks_Projects");
             });
 
             OnModelCreatingPartial(modelBuilder);
--- ManagemAntsServer/DataAccess/Project.cs
+++ ManagemAntsServer/DataAccess/Project.cs
@@ -7,9 +7,16 @@ namespace ManagemAntsServer.DataAccess
 {
     public partial class Project
     {
+        public Project()
+        {
+            Tasks = new HashSet<Task>();
+        }
+
         public long Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public long Owner { get; set; }
+
+        public virtual ICollection<Task> Tasks { get; set; }
     }
 }

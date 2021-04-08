72b5e2380d17c1c6dddb04776489367ef284939c
J3remiez <32276543+J3remiez@users.noreply.github.com>
2021-04-08 12:19:17 +0200

    add navbar in project html

---
+++ ManagemAntsClient/Controllers/ProjectController.cs
@@ -0,0 +1,54 @@
+﻿using ManagemAntsClient.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ManagemAntsClient.Controllers
+{
+    public class ProjectController : Controller
+    {
+        private readonly ILogger<ProjectController> _logger;
+        private readonly string test;
+        private string url = "https://localhost:44352/api/Project";
+
+        static HttpClient client = new HttpClient();
+        public ProjectController(ILogger<ProjectController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            /*            client.BaseAddress = new Uri(url);
+                        client.DefaultRequestHeaders.Accept.Add(
+                        new MediaTypeWithQualityHeaderValue("application/json"));
+
+                        HttpResponseMessage responce = client.GetAsync("").Result;
+
+                        IEnumerable<Project> projects = null;
+                        if (responce.IsSuccessStatusCode)
+                            projects = await JsonSerializer.DeserializeAsync<IEnumerable<Project>>(await responce.Content.ReadAsStreamAsync());
+                        return View(new Projects(projects));*/
+            return View(new ProjectPage() { project = new Project() { id = 1, name = "Ouistiti", description = "Un ptit singe tout mimi" }, loggedUser = "Kaijo" }) ;
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}

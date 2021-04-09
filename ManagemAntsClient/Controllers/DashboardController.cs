c16b1689f4fbcab3cadd2e76b4883fb297a0b01d
J3remiez <32276543+J3remiez@users.noreply.github.com>
2021-04-09 14:06:42 +0200

    add view dashboard and controller

---
+++ ManagemAntsClient/Controllers/DashboardController.cs
@@ -0,0 +1,105 @@
+﻿using ManagemAntsClient.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ManagemAntsClient.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private string url = "https://localhost:44352/";
+
+        static HttpClient client = new HttpClient();
+        // GET: DashboardController
+        public async Task<ActionResult> Index()
+        {
+            client.BaseAddress = new Uri(url + "/api/ProjectsHasUser/user/1");
+            client.DefaultRequestHeaders.Accept.Add(
+            new MediaTypeWithQualityHeaderValue("application/json"));
+
+            HttpResponseMessage responce = client.GetAsync("").Result;
+
+            IEnumerable<Project> projects = null;
+            if (responce.IsSuccessStatusCode)
+                projects = await JsonSerializer.DeserializeAsync<IEnumerable<Project>>(await responce.Content.ReadAsStreamAsync());
+
+            var dashboard = new DashboardPage() { Projects = new Projects(projects), LoggedUser = new Models.User() { } }
+            return View();
+        }
+
+        // GET: DashboardController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: DashboardController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: DashboardController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DashboardController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: DashboardController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DashboardController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: DashboardController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ ManagemAntsClient/Models/DashboardPage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ManagemAntsClient.Models
+{
+    public class DashboardPage
+    {
+        public User LoggedUser;
+        public Projects Projects;
+    }
+}

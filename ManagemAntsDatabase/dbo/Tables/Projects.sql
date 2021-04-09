e53973a8a204f6e8fdce095e39c605eff17e3f7f
Corentin Le Guennec <corentin.le-guennec@epita.fr>
2021-04-09 13:14:53 +0200

    [DB] updat DataBase scheme

--- ManagemAntsDatabase/dbo/Tables/Projects.sql
+++ ManagemAntsDatabase/dbo/Tables/Projects.sql
@@ -2,10 +2,10 @@
     [id]          BIGINT         IDENTITY (1, 1) NOT NULL,
     [name]        VARCHAR (100)  NOT NULL,
     [description] VARCHAR (5000) NULL,
-    [owner]       BIGINT         NOT NULL,
-    CONSTRAINT [PK_Projects] PRIMARY KEY CLUSTERED ([id] ASC),
-    CONSTRAINT [FK_Projects_Users] FOREIGN KEY ([owner]) REFERENCES [dbo].[Users] ([id])
+    CONSTRAINT [PK_Projects] PRIMARY KEY CLUSTERED ([id] ASC)
 );
+
+
 
 
 
--- ManagemAntsServer/DataAccess/Repositories/IRepository.cs
+++ ManagemAntsServer/DataAccess/Repositories/IRepository.cs
@@ -9,8 +9,8 @@ namespace ManagemAntsServer.DataAccess.Repositories
     public interface IRepository<DBEntity, ModelEntity>
 
     {
-        Task<IEnumerable<ModelEntity>> GetAll(params Expression<Func<DBEntity, object>>[] includes);
-        Task<IEnumerable<ModelEntity>> GetByPredicate(Func<DBEntity, bool> predicate, params Expression<Func<DBEntity, object>>[] includes);
+        IEnumerable<ModelEntity> GetAll(params Expression<Func<DBEntity, object>>[] includes);
+        IEnumerable<ModelEntity> GetByPredicate(Func<DBEntity, bool> predicate, params Expression<Func<DBEntity, object>>[] includes);
         Task<ModelEntity> Insert(ModelEntity entity);
         Task<ModelEntity> Update(ModelEntity entity);
         Task<bool> Delete(long idEntity);

920318a52afd62959049ce2ba8c1cc8f9fdac1ee
J3remiez <32276543+J3remiez@users.noreply.github.com>
2021-04-19 16:05:59 +0200

    front connexion done

---
+++ ManagemAntsClient/Controllers/ConnexionController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ManagemAntsClient.Controllers
+{
+    public class ConnexionController : Controller
+    {
+        // GET: ConnexionController
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
